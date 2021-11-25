046fa1a08d9a672fd9609a58e437c04b7cadb6d6
gitrick2 <94807939+gitrick2@users.noreply.github.com>
2021-11-25 17:42:39 -0500

    Update script.js

--- script.js
+++ script.js
@@ -16,7 +16,8 @@ function compute()
         alert("Enter a positive number");
         return;
     }
-    document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+amount+",\<br\>in the year "+year+"\<br\>";
+    document.getElementById("result").innerHTML=ans;
+    //document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+amount+",\<br\>in the year "+year+"\<br\>";
 }
 function updateRate() 
 {
