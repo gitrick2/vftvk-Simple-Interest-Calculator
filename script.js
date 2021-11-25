abca650f24ee1e7c1eda80f99d738b962b14dc06
gitrick2 <94807939+gitrick2@users.noreply.github.com>
2021-11-25 17:37:10 -0500

    Update script.js

--- script.js
+++ script.js
@@ -11,9 +11,14 @@ function compute()
         + "at an interest rate of " + String(rate) + ".<br>"
         + "You will receive an amount of " + String(interest) + ",<br>"
         + "in the year " + String(year);
+    if(principal<=0)
+    {
+        alert("Enter a positive number");
+    }
+    else {
     document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+amount+",\<br\>in the year "+year+"\<br\>"
+    }
 }
-        
 function updateRate() 
 {
     var rateval = document.getElementById("rate").value;
